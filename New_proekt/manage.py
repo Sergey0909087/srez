ebac7555ee2eb613c8b5aba092de4b36dede938f
Sergey0909087 <128168133+Sergey0909087@users.noreply.github.com>
2023-10-31 12:12:01 +0300

    Add files via upload

---
+++ New_proekt/manage.py
@@ -0,0 +1,38 @@
+# word = 'hello'
+# if word.isalpha():
+#    print('Да, буква')
+
+# some_string = 'new_string'
+# print(some_string.center(50, '*')) #viravnivanie po centru
+# new_string = '\t\t\tsome\t str\ting'
+# print(new_string)
+# print(new_string.expandtabs()) #zamena tabulyaciei na probeli
+# print(some_string.ljust(50, '*')) #viravnivanie po levomu kraiu
+# print(some_string.rjust(50)) #viravnivanie po pravomu kraiu
+# print(some_string)
+# print(some_string.lstrip()) #udalenie simvolov sleva
+# print(some_string.rstrip('ing')) #udalenie simvolov sprava 
+# print(some_string.strip()) #udalenie simvolov s obeih storon
+
+#Srezi strok
+#string[a:b]
+
+str_1 = 'abcdef'
+# print(str_1[2:5])
+# print(str_1[-4: -1])
+# print(str_1[-4:5])
+# print(str_1[:3])
+# print(str_1[:-2])
+# print(str_1[2:])
+# print(str_1[-3:])
+# print(str_1[:])
+
+# # string[a:b:k]
+
+# print(str_1[::2])
+# print(str_1[::-1])
+
+# for char in str_1:
+#     print(char)
+
+# print(len(str_1))
